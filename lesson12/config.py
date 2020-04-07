315341cabc2a410e41a32eeebcaff06daefa7669
orusalov <49070339+orusalov@users.noreply.github.com>
2020-04-07 18:24:52 +0300

    Delete config.py
    delete old config

--- lesson12/config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = '910175304:AAHA0pWwx1auhvp60_WCGOvQLjHvjIm0W1M'
-
-BUTTONS = {
-    'go_to_inline': 'Нажми на меня, чтобы ывыа',
-    'contact': 'your number',
-    'location': 'your location'
-}
